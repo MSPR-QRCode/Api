7737c51d063b0f463808c6c2b7b14a58d829b621
Csmeau4 <57441678+Csmeau4@users.noreply.github.com>
2021-03-08 12:31:31 +0100

    test basique

--- tests/ApiControllerTest.php
+++ tests/ApiControllerTest.php
@@ -15,4 +15,31 @@ class ApiControllerTest extends WebTestCase
 
         $this->assertEquals(405, $client->getResponse()->getStatusCode());
     }
+
+    public function GetQrCodeTestPost()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/');
+
+        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    }
+
+    public function GetQrCodeTestPut()
+    {
+        $client = static::createClient();
+
+        $client->request('PUT', '/');
+
+        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    }
+
+    public function GetQrCodeTestDelete()
+    {
+        $client = static::createClient();
+
+        $client->request('DELETE', '/');
+
+        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    }
 }
