69562f28a04d1030d6e1a5a64222dff6ad7c3280
MorganMBA <mba@intechinfo.fr>
2021-03-08 12:39:55 +0100

    test GetPromotions

--- tests/ApiControllerTest.php
+++ tests/ApiControllerTest.php
@@ -3,16 +3,54 @@
 // tests/ApiControllerTest.php
 namespace App\Tests;
 
+use App\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
-    public function rootTest()
+    // public function rootTest()
+    // {
+    //     $client = static::createClient();
+
+    //     $client->request('get', '/');
+
+    //     $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    // }
+
+    public function GetPromotionsWhenClientGet()
     {
         $client = static::createClient();
 
-        $client->request('GET', '/');
+        $client->request('GET', '/api/promotions/');
 
         $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    }
+
+    public function GetPromotionsWhenClientPut()
+    {
+        $client = static::createClient();
+
+        $client->request('put', '/api/promotions/');
+
+        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    }
+
+    public function GetPromotionsWhenClientDelete()
+    {
+        $client = static::createClient();
+
+        $client->request('delete', '/api/promotions/');
+
+        $this->assertEquals(405, $client->getResponse()->getStatusCode());
+    }
+
+    public function GetPromotionsWhenApiTokenIsNull()
+    {
+
+    }
+
+    public function GetPromotionsWhenApiTokenIsNotNull()
+    {
+
     }
 }
